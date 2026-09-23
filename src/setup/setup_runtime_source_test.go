d127b86f436105be0a4181ab051b3b1f93d34bdf
ukyovfx <ukyovfx@gmail.com>
2026-09-24 06:13:54 +0900

    test: migrate Production table in lookup state case

--- src/setup/setup_runtime_source_test.go
+++ src/setup/setup_runtime_source_test.go
@@ -77,6 +77,9 @@ func TestSetupDistinguishesEmptyProductionResultFromLookupFailure(t *testing.T) 
 	t.Setenv("KitsuJWTToken", "")
 	t.Setenv("DISCORD_BOT_TOKEN", "")
 	db := newRuntimeCredentialTestDB(t)
+	if err := db.AutoMigrate(&model.Project{}); err != nil {
+		t.Fatal(err)
+	}
 	model.SetSetting(db, "kitsu.hostname", server.URL)
 	model.SetSetting(db, KitsuAPIBaseURLSettingKey, server.URL)
 	if err := setRuntimeKitsuToken(db, "persisted-kitsu-token"); err != nil {
